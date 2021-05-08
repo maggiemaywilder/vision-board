e085cfe47d3929dc7f2e078ed190448ab6957763
Ian Von Fange <wolfgangvonfange@gmail.com>
2021-05-08 12:25:23 -0500

    Added delete routes client-side

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -10,12 +10,48 @@ export default {
   getBoard: function(id) {
     return axios.get("/api/board/" + id);
   },
-  // Deletes the book with the given id
+  // Gets all users
   getUsers: function() {
     return axios.get("/api/users/")
   },
-  // Saves a book to the database
+  // Gets user with given uid
   getUser: function(uid) {
     return axios.get("/api/books" + uid);
+  },
+  // Add a new user to the database  
+  newUser: function(userData) {
+    return axios.post('/api/users', userData);
+  },
+  // Add a new board to the database
+  newBoard: function(uid) {
+      return axios.post('/api/boards', uid);
+  },
+  // Update a board with given id with data  
+  updateBoard: function(bid, data) {
+      return axios.put(`/api/boards/${bid}`, data);
+  },
+  // Add a new link
+  newLink: function(linkData) {
+      return axios.post('/api/links', linkData);
+  },
+  // Add a new image
+  newImage: function(imgData) {
+      return axios.post('/api/images', imgData);
+  },
+  // Add a new tag
+  newTag: function(mediaId, tagData) {
+      return axios.post(`/api/tags/${mediaId}`, tagData)
+  },
+  // Delete a link
+  deleteLink: function(linkId) {
+      return axios.delete(`/api/links/${linkId}`)
+  },
+  // Delete an image
+  deleteImg: function(imgId) {
+    return axios.delete(`/api/links/${imgId}`)
+  },
+  // Delete a tag
+  deleteTag: function(tagId) {
+    return axios.delete(`/api/links/${tagId}`)
   }
 };
