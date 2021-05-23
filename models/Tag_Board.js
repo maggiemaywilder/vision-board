ea6c7df245b0c656395e92d5f03d9423d4f6902e
Maggie May Wilder <maggiemaywilder@gmail.com>
2021-05-22 19:37:45 -0500

    remove duplication for manually entered links

--- models/Tag_Board.js
+++ models/Tag_Board.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
 
     // because upload, image, and manual entry all belong to a board, when a tag is added to one of them it should also be added to the board. trying to look to possible future development and searching based on tags
     Tag_Board.associate = (models) => {
-        Tag_Board.belongsToMany(models.ManualEntry, { through: 'Manual_bridge' });
+        Tag_Board.belongsToMany(models.Link, { through: 'Link_bridge' });
         Tag_Board.belongsToMany(models.Upload, { through: 'Upload_bridge' });
         Tag_Board.belongsToMany(models.Image, { through: 'Image_bridge' });
     };
